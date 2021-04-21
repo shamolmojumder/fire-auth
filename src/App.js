23f9d56657c5bc50265c7a6ffb0b8d8345c43b8c
shamolmojumder <shamolmojumder14@gmail.com>
2021-04-22 00:23:33 +0600

    41.8 Not google user login, signout user

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import firebase from "firebase/app";
 import "firebase/auth";
@@ -7,12 +7,45 @@ import firebaseConfig from './firebase.Config'
 firebase.initializeApp(firebaseConfig)
 
 function App() {
+  const [user,setUser]=useState({
+    isSignedIn:false,
+    name:'',
+    email:'',
+    photo:''
+  })
+  const {name,email,photo}=user;
   const provider = new firebase.auth.GoogleAuthProvider();
   const handleSignIn= ()=>{
     firebase.auth().signInWithPopup(provider)
     .then(res=>{
       const {displayName,email,photoURL}=res.user;
-      console.log(displayName,email,photoURL);
+      const signedinUser={
+        isSignedIn:true,
+        name:displayName,
+        email:email,
+        photo:photoURL
+      }
+      setUser(signedinUser)
+      //console.log(displayName,email,photoURL);
+    })
+    .catch(err=>{
+      console.log(err);
+      console.log(err.message);
+    })
+  }
+  const handleSignOut = ()=>{
+    firebase.auth().signOut()
+    .then(res=>{
+     const signOutUser={
+      isSignedIn:false,
+      name:'',
+      email:'',
+      photo:''
+     }
+     setUser(signOutUser)
+    })
+    .catch(err=>{
+
     })
   }
   
@@ -20,7 +53,17 @@ function App() {
   return (
     <div className="App">
       <h1>Hello from App.js </h1>
-      <button onClick={handleSignIn}>Sign In</button>
+     {
+        user.isSignedIn ? <button onClick={handleSignOut}>Sign out</button> : <button onClick={handleSignIn}>Sign in</button>
+     }
+      {
+        user.isSignedIn && 
+        <div>
+            <p> {name} </p>
+            <p>{email} </p>
+            <img src={photo} alt=""/>
+        </div>
+      }
     </div>
   );
 }

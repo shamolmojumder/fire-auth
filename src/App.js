a995bf566924a4d6cc51e51c89fe3e8c920e81e4
shamolmojumder <shamolmojumder14@gmail.com>
2021-04-26 16:59:19 +0600

    Update State from Form Field

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React, { useState } from 'react';
 import './App.css';
 import firebase from "firebase/app";
 import "firebase/auth";
-import firebaseConfig from './firebase.Config'
+import firebaseConfig from './firebase.Config';
 
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   const [user,setUser]=useState({
     isSignedIn:false,
     name:'',
     email:'',
+    password:'',
     photo:''
-  })
+  });
   console.log(user);
   const {name,email,photo}=user;
   const provider = new firebase.auth.GoogleAuthProvider();
@@ -26,15 +27,15 @@ function App() {
         name:displayName,
         email:email,
         photo:photoURL
-      }
-      setUser(signedinUser)
+      };
+      setUser(signedinUser);
       //console.log(displayName,email,photoURL);
     })
     .catch(err=>{
       console.log(err);
       console.log(err.message);
-    })
-  }
+    });
+  };
   const handleSignOut = ()=>{
     firebase.auth().signOut()
     .then(res=>{
@@ -43,23 +44,30 @@ function App() {
       name:'',
       email:'',
       photo:''
-     }
-     setUser(signOutUser)
+     };
+     setUser(signOutUser);
     })
     .catch(err=>{
 
-    })
+    });
   }
 
 const handleBlur = (e)=>{
-  console.log(e.target.name,e.target.value);
+  //console.log(e.target.name,e.target.value);
+  let isFormValid= true;
+  debugger;
   if (e.target.name === 'email') {
-    const isEmailValid =  /\S+@\S+\.\S+/.test(e.target.value)
-    console.log(isEmailValid);
-  }if (e.target.name === 'password') {
+    isFormValid =  /\S+@\S+\.\S+/.test(e.target.value);
+  }
+  if (e.target.name === 'password') {
     const isPasswordValid = e.target.value.length>6;
-    const passwordHasNumber=/\d{1}/.test(e.target.value)
-    console.log(isPasswordValid && passwordHasNumber);
+    const passwordHasNumber=/\d{1}/.test(e.target.value);
+    isFormValid = isPasswordValid && passwordHasNumber;
+  }
+  if (isFormValid) {
+    const newuserInfo={...user};
+    newuserInfo[e.target.name]=e.target.value;
+    setUser(newuserInfo);
   }
 }
 
@@ -83,7 +91,14 @@ const handleSubmit =()=>{
 
      
         <h1>Our own Authentication</h1>
+        
+        <p>Name: {user.name} </p>
+        <p>email: {user.email} </p>
+        <p>password: {user.password} </p>
         <form action="" onSubmit={handleSubmit}>
+
+          <input type="text" name="name" id="" onBlur={handleBlur} placeholder="name" required/>
+          <br/>
           <input type="text" name="email" id="" onBlur={handleBlur} placeholder="your email" required/>
           <br/>
           <input type="password" name="password" id="" onBlur={handleBlur} placeholder="password" required/>
